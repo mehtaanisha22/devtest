e14ff7e2db90254c75f0b8fd239fabff92c0f268
anisha <mehataanisha22@gmail.com>
2021-09-14 11:16:24 +0530

    Initial commit

---
+++ src/main/java/com/thevirtugroup/postitnote/model/Note.java
@@ -0,0 +1,42 @@
+package com.thevirtugroup.postitnote.model;
+
+import java.util.Date;
+
+public class Note {
+    private Long id;
+    private String name;
+    private String text;
+    private Date createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
